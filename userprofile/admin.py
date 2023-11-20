1ca7da950298f29f5b1079d49dac03778b1879b6
Bowlesy666 <>
2023-11-20 04:57:51 +0000

    Update admin.py/models.py

--- userprofile/admin.py
+++ userprofile/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+
+from .models import UserProfile, Review
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for UserProfile model.
+    """
+    list_display = ('company_name', 'user_contact_number')
+    search_fields = ['company_name']
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for Review model.
+    """
+    list_display = ('comment', 'stars')
+    search_fields = ['profile_reviewed', 'reviewed_from']
+
+
+class UserProfileInline(admin.StackedInline):
+    """
+    Inline admin configuration for UserProfile model.
+    """
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'company_name'
+
+
+class UserAdmin(BaseUserAdmin):
+    """
+    Admin configuration for the User model with UserProfile inline.
+    """
+    inlines = (UserProfileInline, )
+
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- userprofile/models.py
+++ userprofile/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 from phonenumber_field.modelfields import PhoneNumberField
 from cloudinary.models import CloudinaryField
+from django.utils.text import slugify
 
 
 BUSINESS_SECTOR_CHOICES = (
@@ -35,38 +37,86 @@ BUSINESS_SECTOR_CHOICES = (
 
 
 class UserProfile(models.Model):
+    """
+    Model representing user profiles for businesses.
+    """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     slug = models.SlugField(max_length=200, unique=True)
     business_sector = models.CharField(
-        max_length=50, choices=BUSINESS_SECTOR_CHOICES, default='other')
+        max_length=50, choices=BUSINESS_SECTOR_CHOICES, default='other',
+        help_text="Select the business sector for the user."
+    )
     company_name = models.CharField(max_length=100)
-    comapny_website = models.URLField(max_length=200)
+    company_website = models.URLField(max_length=200, null=True)
     company_contact_number = PhoneNumberField(
-        unique=True, region='GB', null=False)
+        unique=True, region='GB', null=False,
+        help_text="Primary contact number for the company."
+    )
     alternative_company_contact_number = PhoneNumberField(
-        unique=True, region='GB')
+        unique=True, region='GB', null=True,
+        help_text="Alternative contact number for the company."
+    )
     company_contact_email = models.EmailField(unique=True)
     company_bio = models.TextField()
     company_services_post = models.TextField()
     company_hero_picture = CloudinaryField('image', default='placeholder')
     user_contact_number = PhoneNumberField(
-        unique=True, region='GB', null=False)
+        unique=True, region='GB', null=False,
+        help_text="Contact number for the user."
+    )
     display_user_email = models.BooleanField(default=True)
     user_about = models.TextField()
     user_profile_img = CloudinaryField('image', default='placeholder')
 
+    def save(self, *args, **kwargs):
+        """
+        Override the save method to auto-generate the slug.
+        """
+        if not self.slug:
+            self.slug = slugify(self.company_name + '-' + str(self.pk))
+        super().save(*args, **kwargs)
+
     def __str__(self):
-        return f'{ self.user.username }: { self.company_name }'
+        return f'Company Name: { self.company_name }'
 
 
 class Review(models.Model):
-    review_from = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='reviewed_from')
-    profile_reviewed = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='profile_reviewed')
-    stars = models.IntegerField()
+    """
+    Model representing reviews for user profiles.
+    """
+    review_from = models.ForeignKey(
+        UserProfile, on_delete=models.CASCADE, related_name='reviewed_from',
+        help_text="User profile giving the review."
+    )
+    profile_reviewed = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='profile_reviewed',
+        help_text="User profile being reviewed."
+    )
+    stars = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)],
+        help_text="Rating given in stars (1 to 5)."
+    )
     comment = models.TextField()
-    date_created = models.DateField()
-    time_created = models.TimeField()
+    date_created = models.DateField(
+        help_text="Date when the review was created."
+    )
+    time_created = models.TimeField(
+        help_text="Time when the review was created."
+    )
 
-    def save(self):
-        user = self.reviewed
-        stars = Review.objects.filter(reviewed=user).aggregate(sum(stars))
+    objects = models.Manager()
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+    @staticmethod
+    def get_average_stars_for_user(user):
+        """
+        Get the average stars for a given user.
+        """
+        return Review.objects.filter(profile_reviewed=user).aggregate(
+            avg_stars=models.Avg('stars')
+        )['avg_stars'] or 0
+
+    def __str__(self):
+        return f"Stars Awarded - {self.stars} stars"
