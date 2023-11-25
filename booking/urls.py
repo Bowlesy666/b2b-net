7baadb223190700aaf84b932f3c81b1944c3c979
Bowlesy666 <>
2023-11-25 13:25:51 +0000

    Add UpdateBookingView MVC

--- booking/urls.py
+++ booking/urls.py
@@ -1,9 +1,11 @@
 from . import views
 from django.urls import path
-from booking.views import CreateBookingView
+from booking.views import CreateBookingView, UpdateBookingView
 
 
 urlpatterns = [
     path('create_booking/', views.CreateBookingView.as_view(), name='create_booking'),
     path('booking_list/', views.BookingListView.as_view(), name='booking_list'),
+    path('update_booking/<slug:slug>/',
+         views.UpdateBookingView.as_view(), name='update_booking'),
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.views import generic, View
 from django.views.generic import DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import UpdateView, CreateView, DeleteView
+from django.db.models import Q
 from userprofile.models import UserProfile
 from .models import Booking
 from .forms import BookingForm
@@ -25,8 +27,32 @@ class CreateBookingView(CreateView):
 
 class BookingListView(generic.ListView):
     """
-    View for listing user profiles.
+    View for listing user bookings.
     """
     model = Booking
     template_name = 'booking_list.html'
     context_object_name = 'booking_list'
+
+    def get_queryset(self):
+        # Get the current user
+        user = self.request.user
+
+        # Filter bookings where the user is either the sender or receiver
+        return Booking.objects.filter(Q(sender=user.userprofile) | Q(receiver=user.userprofile))
+
+
+class UpdateBookingView(LoginRequiredMixin, UpdateView):
+    """
+    View for updating a 1-2-1 meeting.
+    """
+    model = Booking
+    template_name = 'update_booking_form.html'
+    form_class = BookingForm
+    context_object_name = 'booking_detail'
+    slug_url_kwarg = 'slug'  # Specify the URL keyword argument for the slug
+
+    def get_object(self, queryset=None):
+        return self.model.objects.get(slug=self.kwargs.get(self.slug_url_kwarg))
+
+    def get_success_url(self):
+        return reverse_lazy('booking_list')
